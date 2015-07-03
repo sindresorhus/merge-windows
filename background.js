9576cffa2b1d6937d0dd9160b35cba6bce2ce12e
Sindre Sorhus <sindresorhus@gmail.com>
2015-07-03 13:47:36 +0200

    support merging `popups` too - fixes #2

--- background.js
+++ background.js
@@ -1,20 +1,34 @@
 'use strict';
 
-chrome.browserAction.onClicked.addListener(function () {
-	chrome.windows.getCurrent(function (win) {
+function getTabIds(cb) {
+	chrome.tabs.query({
+		currentWindow: false,
+		windowType: 'normal'
+	}, function (tabs) {
 		chrome.tabs.query({
 			currentWindow: false,
-			windowType: 'normal'
-		}, function (tabs) {
-			var tabIds = tabs.map(function (el) {
-				return el.id;
+			windowType: 'popup'
+		}, function (tabs2) {
+			var tabIds = tabs.concat(tabs2).map(function (x) {
+				return x.id;
 			});
 
-			chrome.tabs.move(tabIds, {index: -1, windowId: win.id}, function () {
+			cb(tabIds);
+		});
+	});
+}
+
+chrome.browserAction.onClicked.addListener(function () {
+	getTabIds(function (tabIds) {
+		chrome.windows.getCurrent(function (win) {
+			chrome.tabs.move(tabIds, {
+				windowId: win.id,
+				index: -1
+			}, function () {
 				// preserve pinned tabs
-				tabs.forEach(function (el) {
-					if (el.pinned) {
-						chrome.tabs.update(el.id, {pinned: true});
+				tabs.forEach(function (x) {
+					if (x.pinned) {
+						chrome.tabs.update(x.id, {pinned: true});
 					}
 				});
 			});
