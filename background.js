e57f045e5dd93fa20ab0879d67b1b2a78e77530b
Federico Brigante <github@bfred.it>
2017-10-09 11:29:30 -0500

    First working version

--- background.js
+++ background.js
@@ -1,23 +1,30 @@
 'use strict';
 
-const windowTypes = ['normal', 'popup'];
-
 const p = (fn, ...args) => new Promise(resolve => fn(...args, resolve));
 
 chrome.browserAction.onClicked.addListener(async () => {
-	const currentWindow = await p(chrome.windows.getCurrent);
-	const allTabs = await p(chrome.tabs.query, {currentWindow: false});
-	
-	const tabs = allTabs.filter(tab => tab.windowType in windowTypes);
-	
+	let [currentWindow, ...tabs] = await Promise.all([
+		p(chrome.windows.getCurrent),
+		p(chrome.tabs.query, {
+			currentWindow: false,
+			windowType: 'normal'
+		}),
+		p(chrome.tabs.query, {
+			currentWindow: false,
+			windowType: 'popup'
+		})
+	]);
+
+	tabs = Array.prototype.concat.apply([], tabs);
+
 	await p(chrome.tabs.move, tabs.map(tab => tab.id), {
 		windowId: currentWindow.id,
 		index: -1
 	});
-	
+
 	for (const tab of tabs) {
-		if (tab.pinned) {		
-			chrome.tabs.update(pinned.id, {pinned: true});
+		if (tab.pinned) {
+			chrome.tabs.update(tab.id, {pinned: true});
 		}
 	}
 });
